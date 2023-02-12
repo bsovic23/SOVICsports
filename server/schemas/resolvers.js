c0ec51351b2cc040faff12a45a8366c66e2b5340
Brit Sovic <bsovic@tulane.edu>
2023-02-12 12:43:22 -0500

    new structure

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -11,37 +11,12 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
 
     Query: {
-        me: async (parent, args, context) => {
-            if (context.user) {
-                const userData = await User.findOne({ _id: context.user._id })
-                .select('-__v -password');
-    
-                return userData;
-            }
-
-            throw new AuthenticationError('Not logged in');
-        },
         // [X] Query works in apollo studio
         // Get All All star Entries
         allNbaAllStar: async (parent, { username }) => {
             const params = username ? { username } : {};
             return nbaAllStar.find().sort({ enteredAt: -1 });
         },
-
-        // Get All Users
-        //
-        users: async () => {
-            return User.find()
-                .select('-__v -password');
-        },
-
-        // Get One User
-        //
-        user: async (parent, { username }) => {
-            return User.findOne({ username })
-                .select('-__v -password');
-        },
-
         // Get All History
         // 
         allHistory: async () => {
@@ -66,63 +41,11 @@ const resolvers = {
     },
 
     Mutation: {
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
-            const token = signToken(user);
-
-            return { user, token };
-        },
-
-        login: async (parent, { username, password }) => {
-            const user = await User.findOne({ username });
-
-            if (!user) {
-                throw new AuthenticationError('Incorrect credentials');
-            }
-
-            const passwordSuccess = await user.isCorrectPassword(password);
-
-            if (!passwordSuccess) {
-                throw new AuthenticationError('Incorrect Password');
-            }
-
-            const token = signToken(user);
-            return { user, token };
-        },
-
         // [X] Mutation works in apollo studio
-        addNbaAllStar: async (parent, args, context) => {
-            if (context.user) {
-
-            const nbaAllStarEntry = await nbaAllStar.create({ ...args, username: context.user.username });
+        addNbaAllStar: async (parent, args) => {
+            const nbaAllStarEntry = await nbaAllStar.create(args);
             
-            await User.findByIdAndUpdate(
-                {_id: context.user._id },
-                { $push : { allNbaAllStar: nbaAllStarEntry._id }},
-                { new: true }
-            );
-
             return nbaAllStarEntry;
-            }
-
-            throw new AuthenticationError('You need to be logged in!');
-        },
-
-        addBowlPickem: async(parent, args, context) => {
-            if (context.user) {
-            
-            const bowlPickemEntry = await bowlPickem.create({...args, username: context.user.username }); 
-
-            await User.findByIdAndUpdate(
-                {_id: context.user._id },
-                { $push : { bowlPickem: bowlPickemEntry._id }},
-                { new: true }
-            );
-
-            return bowlPickemEntry;
-            }
-
-            throw new AuthenticationError('You need to be logged in!');
         },
 
         addHistory: async(parent, args) => {
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,16 +1,6 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    
-    type User {
-        _id: ID
-        username: String
-        firstname: String
-        lastname: String
-        password: String
-        favoriteSport: String
-        favoriteTeam: String
-    }
 
     type nbaAllStar {
         _id: ID
@@ -50,15 +40,7 @@ const typeDefs = gql`
         totalpx: String
     }
 
-    type Auth {
-        token: ID!
-        user: User
-    }
-
     type Query {
-        me: User
-        users: [User]
-        user(username: String): User
         allNbaAllStar(username: String): [nbaAllStar]
         allHistory: [history]
         historyUsername(username: String): [history]
@@ -67,8 +49,6 @@ const typeDefs = gql`
     }
 
     type Mutation {
-        login(username: String, password: String): Auth
-        addUser(username: String, firstname: String, lastname: String, password: String, favoriteSport: String, favoriteTeam: String): Auth
         addNbaAllStar(starterOne: String, starterTwo: String, starterThree: String, starterFour: String, benchOne: String, benchTwo: String, benchThree: String, benchFour: String, skillsChamp: String, threeChamp: String, dunkChamp: String, challengeCaptain: String): nbaAllStar
         addBowlPickem(year: String, game1: String, game2: String, game3: String): bowlPickem
         addHistory(username: String, year: String, challenge: String, score: String, place: String, totalpx: String): history
