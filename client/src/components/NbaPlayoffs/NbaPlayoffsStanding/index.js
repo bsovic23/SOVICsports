1edbedd6adcf93c4b23fd89779a5515108405ca8
Brit Sovic <bsovic@tulane.edu>
2023-04-28 10:35:28 -0400

    updated standings

--- client/src/components/NbaPlayoffs/NbaPlayoffsStanding/index.js
+++ client/src/components/NbaPlayoffs/NbaPlayoffsStanding/index.js
@@ -4,18 +4,51 @@ import NbaPlayoffsNavbar from '../../Navbar/nbaPlayoffsNavbar';
 import { sortByScore } from '../../../utils/functions';
 
 function NbaPlayoffsStandings() {
+    const userEntries = [
+        { userName: 'Brit Sovic'},
+    ];
+
+    const playerScores = [
+        { playerName: 'Lebron', score: '114'},
+        { playerName: 'Tatum', score: '133'},
+        { playerName: 'Giannis', score: '70'},
+        { playerName: 'Jokic', score: '136'},
+        { playerName: 'Embiid', score: '65'},
+        { playerName: 'Randle', score: '77'},
+        { playerName: 'Mitchell', score: '116'},
+        { playerName: 'Curry', score: '157'},
+        { playerName: 'KD', score: '147'},
+        { playerName: 'Booker', score: '191'},
+        { playerName: 'CP3', score: '73'},
+        { playerName: 'Bane', score: '126'},
+        { playerName: 'Ja', score: '113'},
+        { playerName: 'KAT', score: '91'},
+        { playerName: 'Quickley', score: '50'},
+        { playerName: 'Brogdon', score: '63'},
+        { playerName: 'Portis', score: '48'},
+        { playerName: 'Poole', score: '69'},
+        { playerName: 'Levert', score: '72'},
+        { playerName: 'Monk', score: '91'},
+        { playerName: 'Westbrook', score: '118'},
+        { playerName: 'Warren', score: '5'},
+        { playerName: 'Melton', score: '48'},
+        { playerName: 'Tyus', score: '24'},
+        { playerName: 'Bruce Brown', score: '62'},
+        { playerName: 'Schroeder', score: '26'}
+    ];
+
     const userScores = [
-        { name: '10fifty', score: 372 },
-        { name: 'Brit Sovic', score: 375 },
-        { name: 'Peter Swain', score: 487 },
-        { name: 'Maloney', score: 482 },
-        { name: 'Ty Sovic', score: 480 },
-        { name: 'Eggy', score: 400 },
-        { name: 'Rob Storey', score: 461 },
-        { name: 'Anthony Wright', score: 467 },
-        { name: 'Alec Zoida', score: 436 },
-        { name: 'Tre', score: 441 },
-        { name: 'Matt Friedman', score: 425 },
+        { name: '10fifty', score: 929 },
+        { name: 'Brit Sovic', score: 965 },
+        { name: 'Peter Swain', score: 1039 },
+        { name: 'Maloney', score: 1105 },
+        { name: 'Ty Sovic', score: 1063 },
+        { name: 'Eggy', score: 952 },
+        { name: 'Rob Storey', score: 1024 },
+        { name: 'Anthony Wright', score: 1042 },
+        { name: 'Alec Zoida', score: 994 },
+        { name: 'Tre', score: 967 },
+        { name: 'Matt Friedman', score: 943 },
     ];
 
     const userStandings = sortByScore(userScores);
@@ -27,7 +60,7 @@ function NbaPlayoffsStandings() {
             </div>
             <h1>NBA Playoffs Challenge Standings</h1>
             <div>
-                <p>Standings updated as of 4/21 9:00 am ET</p>
+                <p>Standings updated as of 4/28 9:31 am ET</p>
             </div>
             <div>
                 <tr>
--- client/src/pages/NbaPlayoffsStandings/index.js
+++ client/src/pages/NbaPlayoffsStandings/index.js
@@ -8,4 +8,4 @@ function NbaPlayoffsStandings() {
             </tr>
         </section>
     )
-}
+};
