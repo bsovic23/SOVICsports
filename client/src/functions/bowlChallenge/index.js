072dab6ada4aafaf847c68cdc0d49f54911181e2
Brit Sovic <bsovic@tulane.edu>
2023-12-18 19:37:39 -0500

    minor standings update

--- client/src/functions/bowlChallenge/index.js
+++ client/src/functions/bowlChallenge/index.js
@@ -1,27 +1,30 @@
 // College Football Bowl Challenge Standings Function
 
 export const bowlChallengeStandings = (data) => {
-    /* const winners = [
-        { winner1: "Georgia", points: 1},
-    ]; */
+    const winners = [
+        { winner1: "Jacksonville State", points: 1},
+        { winner3: "Appalachian State", points: 1},
+        { winner17: "UCLA", points: 2},
+        { winner18: "Texas Tech", points: 2},
+    ];
 
-    const winners = []; // blank until winners updated
     let standings = [];
 
-    for (const entries of data) {
+    for (const entry of data) {
         let totalPoints = 0;
 
-        for (let i=0; i < winners.length; i++) {
-                const pickChoice = entries[`game${i+1}`];
-                const actualWinner = winners[i][`winner${i + 1}`];
+        for (const winner of winners) {
+            const gameNumber = parseInt(Object.keys(winner)[0].replace('winner', ''), 10);
+            const pickChoice = entry[`game${gameNumber}`];
+            const actualWinner = winner[`winner${gameNumber}`];
 
-                if (pickChoice === actualWinner) {
-                    totalPoints += winners[i].points;
-                } else continue;
+            if (pickChoice === actualWinner) {
+                totalPoints += winner.points;
             }
-        
-        standings.push({entryPerson: entries.entryName, points: totalPoints, champion: entries.champion});
         }
+
+        standings.push({ entryPerson: entry.entryName, points: totalPoints, champion: entry.champion });
+    }
     
     standings.sort((a,b) => b.points - a.points);
     
